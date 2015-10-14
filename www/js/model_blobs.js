015af57ec08e7dbb0588a1747d1bc553e4925b17
Brian Lamb <bl@bleedingedgewebsites.com>
2015-10-13 18:36:54 -0700

    adjusted paths on paths var moved app.js to parent root renamed some tasks, added server-js task gulp is now linting root also, which covers the gulpfile and app.js added font awesome to repo - incomplete added gzip compression to the server corrected ignore file to accomodate move from publib/ build dir to www/ build dir

--- www/js/model_blobs.js
+++
@@ -1,1 +0,0 @@
-var mongoose=require("mongoose"),blobSchema=new mongoose.Schema({name:String,badge:Number,dob:{type:Date,"default":Date.now},isloved:Boolean});mongoose.model("Blob",blobSchema);
--- www/js/model_db.js
+++
@@ -1,1 +0,0 @@
-var mongoose=require("mongoose");mongoose.connect("mongodb://127.0.0.1/mmdb");
